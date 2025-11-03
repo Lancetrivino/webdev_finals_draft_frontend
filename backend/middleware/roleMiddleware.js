e51ddfe32114e34b8d4c3a91256a3cc226ed80b6
Lance <lancetrivino30@gmail.com>
2025-11-03 18:18:01 +0800

    chore: remove duplicate node modules

--- backend/middleware/roleMiddleware.js
+++
@@ -1,5 +0,0 @@
-export const adminOnly = (req, res, next) => {
-  if (req.user.role !== "Admin")
-    return res.status(403).json({ message: "Admin access only" });
-  next();
-};
--- backend/routes/feedbackRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { protect } from "../middleware/authMiddleware.js";
-import { addFeedback } from "../controllers/feedbackController.js";
-
-const router = express.Router();
-
-router.post("/", protect, addFeedback);
-
-export default router;
