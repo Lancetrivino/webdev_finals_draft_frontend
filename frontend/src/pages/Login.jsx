ec244db278a2be3a720b82409a1d66b7ee2b27fc
Lance <lancetrivino30@gmail.com>
2025-11-12 02:17:50 +0800

    still fixing issue

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useAuth, AuthProvider } from "../contexts/AuthContext";
+import { useAuth, AuthProvider } from "./contexts/AuthContext";
 
 function Login() {
   const { login } = useAuth();
